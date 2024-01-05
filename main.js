98a4b2f7672484a5da58bd93daaca51ab4e2b712
Matias Ary Veron <veron-matias@hotmail.com>
2024-01-05 20:22:16 -0300

    prentrega

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-let yn= prompt ("Bienvenido al cuestionario sobre Lionel Messi. Estas listo?");
+let nombre= prompt("Ingrese nombre")
+alert (`Hola ${nombre} bienvenido al cuestionario sobre Lionel Messi`)
+
+let yn= prompt ("Estas listo? Si/No").toLowerCase();
+while (yn !="si"){
+alert("Adios");
+yn= prompt ("Bienvenido al cuestionario sobre Lionel Messi. Estas listo?").toLowerCase();
+}
+alert ("Comencemos!")
 
 let respuesta= prompt ("Ingrese el nombre del futbolista Messi").toLowerCase();
-if (respuesta =="lionel") {
-    alert ("Correcto, Lionel Andres Messi")    
+if (respuesta ==="lionel" && respuesta !="") {
+    alert (`Correcto ${nombre} ! La respuesta es Lionel Andres Messi`)    
 } else {
-    alert ("Respuesta Incorrecta")
+    alert (`Respuesta Incorrecta ${nombre} =( Vuelve a intentarlo`);
 }
 
 let pregunta = prompt("De que nacionalidad es Messi?");
@@ -12,5 +20,7 @@ while (pregunta !== "argentino" && pregunta !== "ARGENTINO" && pregunta !== "Arg
     alert ("Respuesta Incorrecta");
     pregunta = prompt ("De que nacionalidad es Messi?");
 }
-alert ("Respuesta correcta");
+alert (`Respuesta correcta ${nombre}`);
 
+const despedida = () => alert(`Gracias por participar, ${nombre}. Hasta luego!`);
+despedida()
