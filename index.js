a486b5f78ffdbd2558aea402dacfc01c45da8b60
Matias Ary Veron <veron-matias@hotmail.com>
2024-02-22 16:22:03 -0300

    Entrega Final

--- index.js
+++ index.js
@@ -7,30 +7,36 @@ const datos = () => {
       contenedor.innerHTML = "",
       datosMostrados = false
   ) : (
-      setTimeout(() => {
-          let div = document.createElement("div");
-          div.innerHTML = `<h2>Nombre: coderhouse</h2>`;
-          contenedor.appendChild(div);
-          datosMostrados = true;
-      }, 1000)
+    fetch("./data.json")
+    .then(response => response.json())
+    .then(data => {
+        setTimeout(() => {
+            let div = document.createElement("div");
+            div.innerHTML = `<h2>Nombre: ${data[0].nombre}</h2>
+                            <h3> Descripcion: ${data[0].descripcion}</h3>`; 
+            contenedor.appendChild(div);
+            datosMostrados = true;
+        }, 1000);
+    })
+    .catch(error => console.error('Error fetching data:', error))
   );
 };
 boton.addEventListener("click", datos)
 let formulario = document.getElementById("formulario");
 formulario.addEventListener("submit", (e)=>{
     e.preventDefault();
-    let usuario = document.getElementById("usuario").value.toLowerCase();
-    localStorage.setItem("usuario", usuario)
-    usuario === "coderhouse"?
+    let nombre = document.getElementById("nombre").value.toLowerCase();
+    localStorage.setItem("nombre", nombre)
+    nombre === "matias ary veron"?
     Swal.fire({
-        title: "Good job!",
-        text: (`Bienvenido ${usuario}`),
+        title: "Aprobo el curso de Javascript",
+        text: (`Bienvenido ${nombre}`),
         icon: "success"
       }):
       Swal.fire({
         icon: "error",
         title: "Oops...",
-        text: (`${usuario} no es valido`)
+        text: (`${nombre} no es valido`)
       });
     
 });
